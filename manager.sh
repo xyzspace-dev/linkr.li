25d25da21a70db34ea6126526c0ba83bbfa93326
xyzjesper <jesper.l.richert@gmail.com>
2024-10-13 13:05:41 +0200

    Refactor manager.sh and package.json
    Update manager.sh to include additional echo statements for better visibility during build and server startup.
    Add "sharp" dependency to package.json for image processing.

--- manager.sh
+++ manager.sh
@@ -1,4 +1,12 @@
+echo ""
+echo ""
 echo "Building and starting the server..."
+echo ""
+echo ""
 npm run build
+echo ""
+echo ""
 echo "Server is running on ${APP_URL}"
+echo ""
+echo ""
 npm run start
