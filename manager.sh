c77e878f94c38c68a50543ec05257c08297095f5
xyzjesper <jesper.l.richert@gmail.com>
2024-10-13 13:10:29 +0200

    Refactor manager.sh script to start the server using npm run dev

--- manager.sh
+++ manager.sh
@@ -1,12 +1,6 @@
 echo ""
 echo ""
-echo "Building and starting the server..."
+echo "Starting the server..."
 echo ""
 echo ""
-npm run build
-echo ""
-echo ""
-echo "Server is running on ${APP_URL}"
-echo ""
-echo ""
-npm run start
+npm run dev
